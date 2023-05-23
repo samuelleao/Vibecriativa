10d87fdfc45e2011eaf4dfa59d135a99baf10580
Samuel da Silva Santos <samuel.santos@topaziopartner.com.br>
2023-05-23 12:06:39 -0300

    Create: Footer - Add React Icons

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ import * as Accordion from '@radix-ui/react-accordion';
 import { AccordionContent, AccordionItem, AccordionTrigger } from '@/components/Accordion';
 import Image from 'next/image'
 import { Project } from '@/components/Project';
+import { FaFacebookF, FaInstagram, FaLinkedinIn, FaTelegram, FaTwitter, FaWhatsapp } from 'react-icons/fa'
 
 const accordionItems = [
   {
@@ -98,7 +99,7 @@ const projects = [
     img: '/projeto3.png',
     link: 'https://www.exemplo.com/projeto3',
     category: 'Landing Page'
-  },{
+  }, {
     id: 3,
     title: 'Projeto 3',
     description: 'Descrição do Projeto 3',
@@ -244,8 +245,8 @@ export default function Home() {
           </div>
         </section>
         <section className='py-28'>
-          <div className="container flex justify-between relative items-start">
-            <div className="w-6/12 grid grid-cols-2 gap-8">
+          <div className="container flex justify-between relative items-start tablet:flex-col-reverse">
+            <div className="w-6/12 grid grid-cols-2 gap-8 tablet:w-full tablet:grid-cols-1 tablet:mt-32">
               {projects.map((project) => (
                 <Project
                   key={project.id}
@@ -257,13 +258,69 @@ export default function Home() {
                 />
               ))}
             </div>
-            <div className="w-5/12 px-8 sticky top-32">
+            <div className="w-5/12 px-8 sticky top-32 tablet:static tablet:w-full tablet:px-0">
               <small className='text-sm font-medium text-gray-900'>Cases e soluções</small>
               <h6 className='text-3xl font-semibold text-gray-900 leading-relaxed my-5'><span className='text-primary-500'>Empresas</span> como a sua estão <span className='text-primary-500'>redesenhando</span>  a forma como projetam</h6>
               <Button primary={true}>Iniciar projeto</Button>
             </div>
           </div>
         </section>
+        <section className={`bg-[url('/footer.jpg')] py-40 bg-fixed bg-no-repeat bg-cover tablet:py-20`}>
+          <div className="container">
+            <div className="max-w-[500px]">
+              <h6 className='text-3xl tablet:text-lg font-bold text-gray-50 leading-relaxed mb-10'>Temos o prazer em contribuir de forma efetiva para o crescimento da sua empresa</h6>
+              <Button primary={true}>Iniciar Projeto</Button>
+            </div>
+          </div>
+        </section>
+        <footer className='py-32 bg-gray-900'>
+          <div className="container flex justify-between tablet:flex-col-reverse">
+            <div className="grid grid-cols-3 w-8/12 tablet:w-full tablet:grid-cols-1 tablet:gap-32 tablet:mt-32">
+              <ul className='space-y-10'>
+                <li className='text-sm font-semibold text-gray-50'>Institucional</li>
+                <li className='text-sm font-medium text-gray-400'>Sobre a vibe</li>
+                <li className='text-sm font-medium text-gray-400'>Serviços</li>
+                <li className='text-sm font-medium text-gray-400'>Portfólio</li>
+                <li className='text-sm font-medium text-gray-400'>Falar com a vibe</li>
+                <li className='text-sm font-medium text-gray-400'>Iniciar projeto</li>
+                <li className='text-sm font-medium text-gray-400'>Nossos Processos</li>
+              </ul>
+              <ul className='space-y-10'>
+                <li className='text-sm font-semibold text-gray-50'>Institucional</li>
+                <li className='text-sm font-medium text-gray-400'>Website</li>
+                <li className='text-sm font-medium text-gray-400'>Landing Page</li>
+                <li className='text-sm font-medium text-gray-400'>Sistema</li>
+                <li className='text-sm font-medium text-gray-400'>Blog</li>
+                <li className='text-sm font-medium text-gray-400'>Aplicativo</li>
+              </ul>
+              <ul className='space-y-10'>
+                <li className='text-sm font-semibold text-gray-50'>Dúvidas</li>
+                <li className='text-sm font-medium text-gray-400'>Falar no whatsapp</li>
+                <li className='text-sm font-medium text-gray-400'>Enviar email</li>
+              </ul>
+            </div>
+            <div className="flex-1">
+              <ul className='flex gap-4 tablet:grid tablet:grid-cols-3 tablet:gap-16 tablet:mb-32'>
+                <li><a href="" className='w-12 h-12 rounded-lg flex items-center justify-center bg-blue-500  hover:bg-blue-600    text-gray-50'><FaFacebookF size={18}/></a></li>
+                <li><a href="" className='w-12 h-12 rounded-lg flex items-center justify-center bg-pink-500  hover:bg-pink-600    text-gray-50'><FaInstagram size={18}/></a></li>
+                <li><a href="" className='w-12 h-12 rounded-lg flex items-center justify-center bg-sky-600   hover:bg-sky-700   text-gray-50'><FaLinkedinIn size={18}/></a></li>
+                <li><a href="" className='w-12 h-12 rounded-lg flex items-center justify-center bg-sky-500   hover:bg-sky-600   text-gray-50'><FaTelegram size={18}/></a></li>
+                <li><a href="" className='w-12 h-12 rounded-lg flex items-center justify-center bg-green-500  hover:bg-green-600    text-gray-50'><FaWhatsapp size={18}/></a></li>
+                <li><a href="" className='w-12 h-12 rounded-lg flex items-center justify-center bg-blue-500  hover:bg-blue-600    text-gray-50'><FaTwitter size={18}/></a></li>
+              </ul>
+              <img src="/logofooter.png" alt="" className='mt-12' />
+              <p className='text-gray-400 text-sm mb-6 mt-12'>Vibecriativa Digital Marketing e Web. ©</p>
+              <p className='text-gray-400 text-sm'>2023 Todos os direitos reservados</p>
+            </div>
+          </div>
+          <div className="container">
+          <ul className='space-y-10 mt-32'>
+                <li className='text-sm font-semibold text-gray-50'>Privacidade</li>
+                <li className='text-sm font-medium text-gray-400'>Política de privacidade</li>
+                <li className='text-sm font-medium text-gray-400'>Gerenciar uso de cookies</li>
+              </ul>
+          </div>
+        </footer>
       </main>
     </Fragment>
   )
